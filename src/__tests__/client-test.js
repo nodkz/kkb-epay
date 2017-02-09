f71223236d725141602a31c710ef595a18a1a7ce
nodkz <pavel.chertorogov@gmail.com>
2017-02-09 15:18:28 +0600

    test(KkbClient): Fix broken tests in node v4 and v5

--- src/__tests__/client-test.js
+++ src/__tests__/client-test.js
@@ -107,7 +107,7 @@ describe('KkbEpayClient', () => {
     it('should reject if provided empty data', () => {
       // $FlowFixMe
       return client._sign().catch(e => {
-        expect(e.message).toContain('a string or a buffer');
+        expect(e.message).toMatch(/string.*buffer/);
       });
     });
 
@@ -156,7 +156,7 @@ describe('KkbEpayClient', () => {
     it('should reject if provided empty data', () => {
       // $FlowFixMe
       return clientSelfSigned._verify().catch(e => {
-        expect(e.message).toContain('a string or a buffer');
+        expect(e.message).toMatch(/string.*buffer/);
       });
     });
 
